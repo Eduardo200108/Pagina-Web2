956b69c4b4319613434e50e9345aaf2a651ef25f
Eduardo200108 <eduardo.19070053@itsmotul.edu.mx>
2023-04-17 19:17:37 -0600

    proyecctoU2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-blue-400 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("¿sabias que? los perros tienen una curiosa manera de saber la hora..") }}
+                    {{ __("Este es mi block de notas") }}
                 </div>
             </div>
         </div>
