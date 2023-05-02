64fc99bdce544709c320b0fc98d060b6cf26bc79
Eduardo200108 <eduardo.19070053@itsmotul.edu.mx>
2023-05-02 17:59:58 -0600

    Notas

--- resources/views/recordatorios/index.blade.php
+++ resources/views/recordatorios/index.blade.php
@@ -1,9 +1,12 @@
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @foreach ($recordatorios as $item)
+<div class="m-5 w-1/12">
     <h1 class="bg-blue-300">{{$item->id}}</h1>
-    <h1 class="bg-blue-300">{{$item->importacia}}</h1>
+    <h1 class="bg-blue-300">{{$item->importancia}}</h1>
     <h1 class="bg-blue-300">{{$item->recordar}}</h1>
-    <h1 class="bg-blue-300">{{$item->Tema_id}}</h1>
+    <h1 class="bg-blue-300">{{$item->created_at}}</h1>
+</div>
+    
     
     
 
