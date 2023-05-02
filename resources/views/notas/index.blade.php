9947399861a7deffce4c3366ca8fe8df9e904d45
Eduardo200108 <eduardo.19070053@itsmotul.edu.mx>
2023-05-02 17:28:34 -0600

    Notas

--- resources/views/notas/index.blade.php
+++ resources/views/notas/index.blade.php
@@ -1,5 +1,8 @@
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @foreach ($notas as $item)
-    <h1>{{$item->titulo}}</h1>
-    <h1>{{$item->resumen}}</h1>
+    <h1 class="bg-blue-300">{{$item->id}}</h1>
+    <h2 class="bg-blue-300">{{$item->titulo}}</h2>
+    <h3 class="bg-blue-700">{{$item->resumen}}</h3>
+    <h4 class="bg-blue-700">{{$item->p_clave}}</h4>
+    <h3 class="bg-blue-700">{{$item->apunte}}</h3>
 @endforeach
--- resources/views/recordatorios/index.blade.php
+++ resources/views/recordatorios/index.blade.php
@@ -1,4 +1,10 @@
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @foreach ($recordatorios as $item)
-    <h1 class="bg-red-500">{{$item->recordar}}</h1>
+    <h1 class="bg-blue-300">{{$item->id}}</h1>
+    <h1 class="bg-blue-300">{{$item->importacia}}</h1>
+    <h1 class="bg-blue-300">{{$item->recordar}}</h1>
+    <h1 class="bg-blue-300">{{$item->Tema_id}}</h1>
+    
+    
+
 @endforeach
