539a041bd83555f49d23fba6838112d74841b325
Eduardo200108 <eduardo.19070053@itsmotul.edu.mx>
2023-05-03 00:53:36 -0600

    Notas

--- resources/views/notas/index.blade.php
+++ resources/views/notas/index.blade.php
@@ -1,8 +1,12 @@
 @vite(['resources/css/app.css', 'resources/js/app.js'])
+<div class="bg-orange-200 h-full w-full p-0 m-0">
 @foreach ($notas as $item)
-    <h1 class="bg-blue-300">{{$item->id}}</h1>
-    <h2 class="bg-blue-300">{{$item->titulo}}</h2>
-    <h3 class="bg-blue-700">{{$item->resumen}}</h3>
-    <h4 class="bg-blue-700">{{$item->p_clave}}</h4>
-    <h3 class="bg-blue-700">{{$item->apunte}}</h3>
+<div class="m-5 w-2/12">
+
+    <h2 class="bg-yellow-200">{{$item->titulo}}</h2>
+    <h3 class="bg-yellow-200">{{$item->resumen}}</h3>
+    <h3 class="bg-green-400">{{$item->apunte}}</h3>
+    <h4 class="bg-green-400">{{$item->created_at}}</h4>
+</div>
 @endforeach
+</div>
