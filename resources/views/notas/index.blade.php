611e7cc723e1307595a424c6fc8664f7376e82f4
Eduardo200108 <eduardo.19070053@itsmotul.edu.mx>
2023-05-03 14:42:30 -0600

    Notas

--- resources/views/notas/index.blade.php
+++ resources/views/notas/index.blade.php
@@ -1,7 +1,8 @@
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 <div class="bg-orange-200 h-full w-full p-0 m-0">
-@foreach ($notas as $item)
-<div class="m-5 w-2/12">
+    <div class="grid grid-cols-5">
+        @foreach ($notas as $item)
+<div class="m-5 p-2 w-full">
 
     <h2 class="bg-yellow-200">{{$item->titulo}}</h2>
     <h3 class="bg-yellow-200">{{$item->resumen}}</h3>
@@ -9,4 +10,6 @@
     <h4 class="bg-green-400">{{$item->created_at}}</h4>
 </div>
 @endforeach
+    </div>
+
 </div>
--- resources/views/recordatorios/index.blade.php
+++ resources/views/recordatorios/index.blade.php
@@ -1,7 +1,8 @@
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 <div class="bg-orange-200 h-full w-full p-0 m-0">
+    <div class="grid grid-cols-4 w-full">
     @foreach ($recordatorios as $item)
- <div class="m-5 w-2/12">
+    <div class="m-5 p-2 w-full">
     
 
     <h1 class="bg-lime-300">{{$item->id}}</h1>
